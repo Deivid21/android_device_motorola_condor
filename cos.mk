70d1c5ede06c09fbdf87b6366dcebde304ceb0f0
razorrahul123 <rahul.kr.sushant@gmail.com>
2018-06-24 15:59:34 +0000

    Cosmic Bringup

--- cos.mk
+++ cos.mk
@@ -1,10 +1,13 @@
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common Cosmic stuff.
+$(call inherit-product, vendor/cos/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosp_condor
+PRODUCT_DEVICE := condor
+PRODUCT_BRAND := motorola
+PRODUCT_MANUFACTURER := motorola
 PRODUCT_RELEASE_NAME := MOTO E
 PRODUCT_MODEL := Moto E
 
